25e4d17c8de2ac1796fad59fdeb2de521e5ec6f5
Rodolfilho <rocharodolfo555@gmail.com>
2025-02-14 18:09:13 -0300

    coletar dados em linhas diferentes

---
+++ Menu.py
@@ -0,0 +1,46 @@
+from database import salvar_dados, carregar_dados
+
+class Menu:
+    def coletarDados(self):
+        tipos = ["id", "especie", "raca", "idade", "sexo", "porte", "cor", "status", "data_chegada"]
+        concatena = ""
+        for i in range(0,9):
+            inf = input(f"informe o {tipos[i]}: ")
+            if inf == "0":
+                inf = " "
+                pass
+            elif inf == "00":
+                inf = " "
+                break
+            concatena += (inf + ",")
+        return concatena
+
+    def menu(self):
+        while True:
+            print("\n=== Sistema de Gestão do Centro de Adoção ===")
+            print("1. Cadastrar Animal")
+            print("2. Listar Animais")
+            print("3. Cadastrar Voluntário")
+            print("4. Listar Voluntários")
+            print("5. Sair")
+            opcao = input("Escolha uma opção: ")
+            
+            if opcao == "1":
+                print("Informe os dados do animal: (digite 0 se tiver a informação e 00 para encerrar)")
+                print("==================================")
+                dados = self.coletarDados()
+                salvar_dados("animais.txt", dados)
+            elif opcao == "2":
+                print("Animais cadastrados:")
+                print(''.join(carregar_dados("animais.txt")))
+            elif opcao == "3":
+                dados = input("Informe os dados do voluntário (id, nome, contato, funcao): ")
+                salvar_dados("voluntarios.txt", dados)
+            elif opcao == "4":
+                print("Voluntários cadastrados:")
+                print(''.join(carregar_dados("voluntarios.txt")))
+            elif opcao == "5":
+                print("Saindo do sistema...")
+                break
+            else:
+                print("Opção inválida! Tente novamente.")
